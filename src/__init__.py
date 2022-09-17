d2952d6b7c860d06b388c1f3e36bd4f067c9ec1f
tatoalo <apogliaghi@gmail.com>
2022-09-17 19:31:14 +0200

    feat: added global logging and env vars as globals

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,13 @@
+import logging
+
 CONFIG_PATH = "configuration.toml"
+STORAGE_PATH = "data/"
+STORAGE_FILENAME = "storage"
+OPERATIONAL_PATH = "watchdog"
+
+logging.addLevelName(10, "DEBUG")
+logging.basicConfig(level=logging.DEBUG)
+
+logging.getLogger("qbittorrentapi").setLevel(logging.INFO)
+logging.getLogger("requests").setLevel(logging.INFO)
+logging.getLogger("urllib3").setLevel(logging.INFO)
