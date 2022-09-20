b55939ceb0ec1e55d0acdc32b23014e60fd6c37d
tatoalo <apogliaghi@gmail.com>
2022-09-20 18:38:17 +0200

    refactor: refactored atexit

--- src/launcher.py
+++ src/launcher.py
@@ -1,28 +1,14 @@
 import atexit
 
-from src import logging
+from synch import synch_storage
 from torrent_manager import TorrentManager
-
-
-@atexit.register
-def synch_storage():
-    logging.info("Synching storage...")
-    try:
-        t.storage.close()
-    except ValueError:
-        logging.info("Shelve has been already synched and closed!")
-    except Exception:
-        logging.info(
-            "storage could not be closed, perhaps wasn't even opened to begin with."
-        )
-
 
 if __name__ == "__main__":
 
     t = TorrentManager()
+    atexit.register(synch_storage, manager=t)
 
-    categories_retrieved = t.config.categories
-    for category in categories_retrieved:
+    for category in t.config._retrieve_categories():
         t.add_to_watchdog(category=category)
 
     t.check_status()
